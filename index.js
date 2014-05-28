64603c669342bfd6670199d479bdf80823892a70
Nick Matantsev <nick.m@myplanetdigital.com>
2014-05-28 18:22:47 -0400

    Initial implementation

---
+++ index.js
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2014 Nick Matantsev
+ * Licensed under the BSD license.
+ */
+
+'use strict';
+
+var parse = require('css-parse');
+
+function isSelectorGlobal(v) {
+    if (v.charAt(0) === '@') {
+        return true;
+    }
+
+    // test for simple tag name/star with possible attached pseudo- or attribute selectors
+    return /^([a-z0-9]+|\*)(::?[a-z-]+|\[[^\]]+\])*$/.test(v);
+}
+
+function checkChildSelectorConforming(report, remainder, topClass, isParentConstrained) {
+    var isConstrained = false,
+        fullBEMPrefix = topClass + '__';
+
+    // @todo prevent sibling selector on top class
+    if (remainder.charAt(0) === '>') {
+        isConstrained = true;
+        remainder = remainder.replace(/^\>\s+/, '');
+    } else if (remainder.charAt(0) === '+') {
+        isConstrained = isParentConstrained; // we are constrained if parent is constrained
+        remainder = remainder.replace(/^\+\s+/, '');
+    }
+
+    // match optional tag name and a class that does not start with dash (i.e. BEM modifier)
+    var childMatch = /^([a-z0-9-]+|\*)?(?:\.((?=[^-])[a-z0-9_-]+))?(.*)$/.exec(remainder);
+
+    var childElement = childMatch[1],
+        childClass = childMatch[2],
+        trailer = childMatch[3];
+
+    if (childClass && !childElement) {
+        if (childClass.charAt(0) !== '_' && childClass.substring(0, fullBEMPrefix.length) !== fullBEMPrefix) {
+            report('child class must have BEM prefix: ".' + childClass + '"');
+        }
+    } else if (childElement && !childClass) {
+        if (!isConstrained) {
+            report('tag-based match must be a direct child or sibling of direct child: "' + remainder + '"');
+        }
+
+        if (childElement === 'div' || childElement === 'span') {
+            report('do not use non-semantic tag name: "' + childElement + '"');
+        }
+    } else {
+        report('must specify either child element or class but not both: "' + remainder + '"');
+    }
+
+    while(/^\S/.test(trailer)) {
+        var modifierMatch = /^(?:\.-[a-z0-9-]+|::?[a-z-]+|\[[^\]]+\])(.*)$/.exec(trailer);
+
+        if (!modifierMatch) {
+            report('invalid modifier: "' + trailer + '"');
+            return;
+        }
+
+        trailer = modifierMatch[1];
+    };
+
+    if (trailer !== '') {
+        checkChildSelectorConforming(report, trailer.replace(/^\s+/, ''), topClass, isConstrained);
+    }
+}
+
+function checkCSS(css, report) {
+    var ast = parse(css, { position: true });
+
+    ast.stylesheet.rules.forEach(function (rule) {
+        if (rule.type !== 'rule') {
+            return;
+        }
+
+        var positionStart = rule.position.start;
+
+        function fullReport(msg) {
+            report(positionStart.line, positionStart.column, msg);
+        }
+
+        rule.selectors.forEach(function (v) {
+            if (isSelectorGlobal(v)) {
+                return;
+            }
+
+            // match optional tag with non-dash-prefixed class and following BEM modifiers, pseudo-classes, attributes
+            var match = /^([a-z0-9]+)?\.((?=[^-])[a-z0-9-]+)(?:\.-[a-z0-9-]+|::?[a-z-]+|\[[^\]]+\])*(?:\s+(.*))?$/.exec(v);
+
+            if (!match) {
+                fullReport('cannot recognize top-level selector match');
+                return;
+            }
+
+            var topElement = match[1],
+                topClass = match[2],
+                childElements = match[3];
+
+            if (topElement) {
+                fullReport('do not use top-level tag match: "' + topElement + '"');
+            }
+
+            if (childElements !== undefined) {
+                checkChildSelectorConforming(fullReport, childElements, topClass);
+            }
+
+            // @todo check position: absolute to make sure constrained (!) parent has position: relative
+        });
+    });
+}
+
+module.exports = checkCSS;
