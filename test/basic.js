1aea60d6741fc8b0b2dedbf152aacaaf0f00ce9d
Nick Matantsev <nick.matantsev@gmail.com>
2015-07-04 12:39:10 -0400

    More basic tests

--- test/basic.js
+++ test/basic.js
@@ -11,4 +11,8 @@ function run(css) {
     return results;
 }
 
-t.same(run(' '), [], 'empty CSS');
+t.same(run(' '), [], 'empty');
+t.same(run('testtag {}'), [], 'global tag');
+t.same(run('.top-level-class {}'), [], 'top-level class');
+
+t.same(run('.-dangling-top-modifier {}'), [[1, 1, 'cannot recognize top-level selector match']], 'dangling top-level modifier');
