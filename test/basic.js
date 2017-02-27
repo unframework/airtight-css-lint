8a58b4119441a9f981842efa395e0068978c757f
Shanly Suepaul <shanlys@gmail.com>
2017-02-27 18:00:14 -0500

    Adding ability for children to directly reference other children

--- test/basic.js
+++ test/basic.js
@@ -16,3 +16,7 @@ t.same(run('testtag {}'), [], 'global tag');
 t.same(run('.top-level-class {}'), [], 'top-level class');
 
 t.same(run('.-dangling-top-modifier {}'), [[1, 1, 'cannot recognize top-level selector match']], 'dangling top-level modifier');
+
+// child element direct child references
+t.same(run('.top-level ._child > h1 {}'), [], 'allow child classes to directly refer to element tags');
+t.same(run('.top-level ._child > div {}'), [[1, 1, 'do not use non-semantic tag name: "div"']], 'child classes can\'t directly refer to non-semantic element tags');
